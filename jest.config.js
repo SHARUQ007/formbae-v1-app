a08b60f2475296af6d37dbe5a443093b5cc0c22c
SHARUQ007 <sharuqazeez@gmail.com>
2026-08-01 20:31:29 +0530

    Resolve app audit and Jest warnings

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   setupFiles: ['./jestSetup.js'],
+  watchman: false,
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation|react-native-vector-icons|react-native-screens|react-native-safe-area-context)/)',
   ],
